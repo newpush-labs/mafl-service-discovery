d8e69b181b406f77dc2664f1dc9123cdb1fd6f54
Laszlo Racz <lracz@newpush.com>
2024-10-12 13:09:54 -0600

    fix

--- mafl-service-discovery.py
+++ mafl-service-discovery.py
@@ -122,10 +122,11 @@ def monitor_docker_events():
                 yield event
                 time.sleep(0.1)  # Adding a delay to prevent 100% CPU usage
 
-        for event in client.events(decode=True, timeout=10):
+        for event in client.events(decode=True):
             if event['Type'] in ['container', 'service']:
                 mafl_services = get_mafl_services()
                 update_config_yaml(mafl_services)
+                time.sleep(0.1)  # Adding a delay to prevent 100% CPU usage
                 # print("config.yml has been updated due to Docker event.")
 
     except Exception as e:
